d930763facf1c48ddeed4c759bc40da789b2b143
taehyun <taehyun@bodycodi.com>
2026-06-17 19:38:57 +0900

    [commerce] 도메인, 서비스 테스트 코드 추가

---
+++ product/src/test/kotlin/com/commercelab/product/application/service/ProductServiceTest.kt
@@ -0,0 +1,86 @@
+package com.commercelab.product.application.service
+
+import com.commercelab.product.application.exception.ProductNotFoundException
+import com.commercelab.product.application.input.CreateProductCommand
+import com.commercelab.product.application.output.LoadProductPort
+import com.commercelab.product.application.output.SaveProductPort
+import com.commercelab.product.domain.model.Product
+import com.commercelab.product.domain.model.ProductStatus
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+
+class ProductServiceTest {
+    private val productRepository = FakeProductRepository()
+    private val productService = ProductService(
+        saveProductPort = productRepository,
+        loadProductPort = productRepository
+    )
+
+    @Test
+    fun `상품 저장 테스트`() {
+        productService.createProduct(
+            command = CreateProductCommand(
+                name = "name",
+                description = "description",
+                price = 1000000
+            )
+        )
+
+        val product = productRepository.productMap.values.single()
+        assertNotNull(product)
+        assertEquals(product.id, 1L)
+        assertEquals(product.name, "name")
+        assertEquals(product.description, "description")
+        assertEquals(product.price, 1000000)
+        assertEquals(product.status, ProductStatus.ACTIVE)
+    }
+
+    @Test
+    fun `상품 조회 테스트`() {
+        productRepository.productMap[1L] = Product.restore(
+            id = 1L,
+            name = "name",
+            description = "description",
+            price = 1000000,
+            ProductStatus.ACTIVE
+        )
+
+        val product = productService.getProduct(id = 1L);
+        assertNotNull(product)
+        assertEquals(product.id, 1L)
+        assertEquals(product.name, "name")
+        assertEquals(product.description, "description")
+        assertEquals(product.price, 1000000)
+    }
+
+    @Test
+    fun `조회할 상품이 없는 경우 예외 발생 테스트`() {
+        assertFailsWith<ProductNotFoundException> {
+            productService.getProduct(id = 1L)
+        }
+    }
+
+    private class FakeProductRepository : SaveProductPort, LoadProductPort {
+        val productMap = mutableMapOf<Long, Product>();
+        private var sequence = 1L;
+
+        override fun save(product: Product) {
+            var id = product.id ?: sequence++
+            productMap[id] = Product.restore(
+                id = id,
+                name = product.name,
+                description = product.description,
+                price = product.price,
+                status = product.status
+            )
+        }
+
+        override fun load(id: Long): Product? {
+            return productMap[id]
+        }
+
+    }
+
+}
---
+++ product/src/test/kotlin/com/commercelab/product/domain/model/ProductTest.kt
@@ -0,0 +1,45 @@
+package com.commercelab.product.domain.model
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class ProductTest {
+
+
+    @Test
+    fun `정상적인 상품 생성 테스트`() {
+        val result = Product.create(
+            name = "name",
+            description = "description",
+            price = 1000000
+        )
+
+        assertEquals("name", result.name)
+        assertEquals("description", result.description)
+        assertEquals(1000000, result.price)
+        assertEquals(ProductStatus.ACTIVE, result.status)
+    }
+
+    @Test
+    fun `상품명이 비어있을때 상품 생성불가 되어야한다`() {
+        assertFailsWith<IllegalArgumentException> {
+            Product.create(
+                name = " ",
+                description = "description",
+                price = 1000000
+            )
+        }
+    }
+
+    @Test
+    fun `금액이 음수로 지정되면 상품 생성 불가 되어야한다`() {
+        assertFailsWith<IllegalArgumentException> {
+            Product.create(
+                name = "name",
+                description = "description",
+                price = -1
+            )
+        }
+    }
+}
